6803b598a7a951fa664857c37749574c67ac4670
Brian Cottingham <spiffytech@gmail.com>
2013-01-08 08:40:35 -0500

    Got the cards stored in JSON

--- jsonify.js
+++ jsonify.js
@@ -1,12 +1,40 @@
 var fs = require('fs');
-var $ = jQuery = require('jQuery');
+var $ = jQuery = require('jquery');
 require('./jquery.csv-0.71.min.js');
 
-var sample = './objectives.csv';
-fs.readFile(sample, 'UTF-8', function(err, csv) {
-  jQuery.csv.toObjects(csv, {}, function(err, data) {
-    for(var i=0, len=data.length; i<len; i++) {
-      console.log(data[i]);
-    }
-  });
+var objectives = [];
+var bonuses = [];
+
+fs.readFile("objectives.csv", 'UTF-8', function(err, csv) {
+    jQuery.csv.toObjects(csv, {}, function(err, data) {
+        for(var i=0, len=data.length; i<len; i++) {
+            var c = data[i].count;
+            for(var j = 0; j < c; j++) {
+                delete data[i].count;
+                if(data[i]["2p"] == "Yes") {
+                    data[i]["2p"] = true;
+                } else {
+                    data[i]["2p"] = false;
+                }
+                objectives.push(data[i]);
+            }
+        }
+
+        var f = fs.openSync("objectives.json", "w");
+        fs.writeSync(f, JSON.stringify(objectives, null, 4));
+        fs.close(f);
+    });
+});
+
+fs.readFile("bonuses.csv", 'UTF-8', function(err, csv) {
+    jQuery.csv.toObjects(csv, {}, function(err, data) {
+        for(var i=0, len=data.length; i<len; i++) {
+            data[i].skulls = parseInt(data[i].skulls);
+            bonuses.push(data[i]);
+        }
+
+        var f = fs.openSync("bonuses.json", "w");
+        fs.writeSync(f, JSON.stringify(bonuses, null, 4));
+        fs.close(f);
+    });
 });
